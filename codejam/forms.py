f533a2c6cac0ebb2d039a5bc5cb6a5cfcd0109b8
Catalina Rodríguez <catal.rodriguezs@duocuc.cl>
2024-07-24 14:11:47 -0400

    4 horas

--- codejam/forms.py
+++ codejam/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Reservation
+from datetime import timedelta
 #comentario ds
 
 class ReservationForm(forms.ModelForm):
@@ -14,6 +15,16 @@ class ReservationForm(forms.ModelForm):
             'end_time': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
             'purpose': forms.TextInput(attrs={'class': 'form-control'}),
         }
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        start_time = cleaned_data.get("start_time")
+        end_time = cleaned_data.get("end_time")
+
+        if end_time and start_time and end_time > start_time + timedelta(hours=4):
+            raise forms.ValidationError("end_time must be within 4 hours of start_time")
+        
+        return cleaned_data    
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -21,3 +32,4 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
--- codejam/models.py
+++ codejam/models.py
@@ -1,6 +1,8 @@
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from datetime import timedelta
 
 class CommonSpace(models.Model):
     name = models.CharField(max_length=100)
@@ -18,5 +20,10 @@ class Reservation(models.Model):
     end_time = models.DateTimeField()
     purpose = models.CharField(max_length=255)
 
+    def save(self, *args, **kwargs):
+        if self.end_time > self.start_time + timedelta(hours=4):
+            raise ValidationError("end_time el tiempo maximo son 4 horas start_time")
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return f"{self.common_space.name} reserved by {self.user.username} from {self.start_time} to {self.end_time}"
+        return f"{self.common_space.name} reservado por {self.user.username} desde {self.start_time} hasta {self.end_time}"
